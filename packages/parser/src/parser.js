ab7f17a008b606a3ff64524e0dd910ba7e68b5f1
Rifat Nabi <to.rifat@gmail.com>
2017-04-16 03:46:28 +1000

    One bind to rule them all :smile:

--- packages/parser/src/parser.js
+++ packages/parser/src/parser.js
@@ -34,25 +34,31 @@ export default class Parser {
   // match one or more occurences of the specified parser
   // :: Parser<A> -> Parser<[A]>
   many1 () {
-    return Parser.of(input => this.parse(input).cata({
-      Failure: Failure,
-      Success: ([firstValue, inputAfterFirstParse]) => {
-        // if first found, look for zeroOrMore now
-        const [subsequentValues, remainingInput] = 
-          this.parseZeroOrMore(inputAfterFirstParse);
-        return Success([[firstValue, ...subsequentValues], remainingInput]);
-      }
-    }));
+    return this.bind(head => this.many().bind(tail => 
+      Parser.return([head, ...tail])));
+    // old implementation without bind
+    // return Parser.of(input => this.parse(input).cata({
+    //   Failure: Failure,
+    //   Success: ([firstValue, inputAfterFirstParse]) => {
+    //     // if first found, look for zeroOrMore now
+    //     const [subsequentValues, remainingInput] = 
+    //       this.parseZeroOrMore(inputAfterFirstParse);
+    //     return Success([[firstValue, ...subsequentValues], remainingInput]);
+    //   }
+    // }));
   }
   
   andThen (otherParser) {
-    return Parser.of(input => this.parse(input).cata({
-      Failure: Failure,
-      Success: ([value1, remaining1]) => otherParser.parse(remaining1).cata({
-        Failure: Failure,
-        Success: ([value2, remaining2]) => Success([[value1, value2], remaining2])
-      })
-    }));
+    return this.bind(p1Result => otherParser.bind(p2Result => 
+      Parser.return([p1Result, p2Result])));
+    // old implementation without bind
+    // return Parser.of(input => this.parse(input).cata({
+    //   Failure: Failure,
+    //   Success: ([value1, remaining1]) => otherParser.parse(remaining1).cata({
+    //     Failure: Failure,
+    //     Success: ([value2, remaining2]) => Success([[value1, value2], remaining2])
+    //   })
+    // }));
   }
 
   orElse (otherParser) {
@@ -61,14 +67,19 @@ export default class Parser {
   
   // :: f:(A -> B) -> Parser<A> -> Parser<B>
   map (f) {
-    return Parser.of(input => this.parse(input).map(
-      ([value, remaining]) => [f(value), remaining]
-    ));
+    return this.bind(x => Parser.return(f(x)));
+    // old implementation without bind
+    // return Parser.of(input => this.parse(input).map(
+    //   ([value, remaining]) => [f(value), remaining]
+    // ));
   }
   
+  // apply a wrapped function to a wrapped value
   // :: Parser<(A -> B)> -> Parser<A> -> Parser<B>
   apply (otherParser) {
-    return this.andThen(otherParser).map(([f, x]) => f(x));
+    return this.bind(f => otherParser.bind(x => Parser.return(f(x))));
+    // old implementation without bind
+    // return this.andThen(otherParser).map(([f, x]) => f(x));
   }
   
   opt () {
@@ -101,6 +112,17 @@ export default class Parser {
   // Parses zero or more occurrences of p separated by sep
   sepBy (sepParser) {
     return this.sepBy1(sepParser).orElse(Parser.return([]));
+  }
+  
+  // "bind" takes a parser-producing function f, and a parser p
+  // and passes the output of p into f, to create a new parser
+  // :: f:(A -> Parser<B>) -> Parser<A> -> Parser<B>
+  bind (f) {
+    return Parser.of(input => this.parse(input).cata({
+      // return error from parser1
+      Failure: Failure,
+      Success: ([value1, remainingInput]) => f(value1).parse(remainingInput)
+    }));
   }
 
   static of (fn) {
